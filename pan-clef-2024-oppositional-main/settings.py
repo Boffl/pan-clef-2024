9ba4bf81f33212ffe22befc09c31914b082bb935
nadiatimoleon <102558021+nadiatimoleon@users.noreply.github.com>
2024-04-04 00:32:07 +0200

    add new functionality for data splitting
    - add function that shuffles and splits original dataset into train and dev sets
    - extend existing methods for data loading to differentiate between full dataset and split dataset

---
+++ pan-clef-2024-oppositional-main/settings.py
@@ -0,0 +1,18 @@
+'''
+definitions of important setting variables (resources etc.)
+create a local settings.py (git ignored) with these variables and their appropriate values
+when introducing new variables, add them to this file with empty or default values and commit
+'''
+
+# DATASETS
+TRAIN_FULL_DATASET_EN = '../data/dataset_en_train_full.json'
+TRAIN_FULL_DATASET_ES = '../data/dataset_es_train_full.json'
+
+TRAIN_DATASET_EN = '../data/dataset_en_train.json'
+TRAIN_DATASET_ES = '../data/dataset_es_train.json'
+
+DEV_DATASET_EN = '../data/dataset_en_dev.json'
+DEV_DATASET_ES = '../data/dataset_es_dev.json'
+
+TEST_DATASET_EN = ''
+TEST_DATASET_ES = ''
