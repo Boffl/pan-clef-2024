068934d2025d6e569b7b3ec342f97a050bc5de5a
nikob <nikolaj.bauer@uzh.ch>
2024-04-09 16:29:14 +0200

    add evaluation script

---
+++ OpenAI/evaluate.py
@@ -0,0 +1,17 @@
+from sklearn.metrics import precision_score, recall_score, f1_score
+import sys
+
+gold_file = sys.argv[1]
+pred_file = sys.argv[2]
+
+def open_label_file(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        labels = [int(label) for label in f]
+    return labels
+
+gold = open_label_file(gold_file)
+pred = open_label_file(pred_file)
+
+print(f"P: {precision_score(gold, pred)}")
+print(f"R: {recall_score(gold, pred)}")
+print(f"F1: {f1_score(gold, pred)}")
