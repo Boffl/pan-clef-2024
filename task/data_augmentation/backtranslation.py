9274372edc73cb633308d17600ad71c038d357e4
nadiatimoleon <102558021+nadiatimoleon@users.noreply.github.com>
2024-04-18 22:41:09 +0200

    update folder structure
    rename task file to enable smooth imports

---
+++ task/data_augmentation/backtranslation.py
@@ -0,0 +1,67 @@
+import sys
+sys.path.append('C:/Users/Nadia Timoleon/Documents/GitHub/pan-clef-2024/task')
+
+import tqdm
+import argparse
+import pandas as pd
+
+from data_tools.pickle_utils import load_pickle_to_df
+
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+
+def load_and_filter_data(data_name):
+    df = load_pickle_to_df(data_name)
+    
+    # only keep sequences with length up to 2000
+    df = df[df['text'].apply(lambda x: len(x) <= 2000)]
+    return df
+
+
+def translate_text(df):
+    # Load the model and tokenizer
+    model_name = "Helsinki-NLP/opus-mt-es-en"
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    translations = []
+    for text in tqdm.tqdm(df['text']):
+        input_ids = tokenizer.encode(text, return_tensors="pt")
+        outputs = model.generate(input_ids)
+        translated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        translations.append(translated_text)
+    
+    # create a new dataframe with the translated text
+    df_translations = pd.DataFrame({"text": translations})
+    # rename the index to 'id'
+    df_translations.index.name = "id"
+
+    return df_translations
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Translate Spanish to English")
+    parser.add_argument("source_lang", choices=["en", "es"], help="Choose the source language to translate from: 'en' or 'es'")
+    args = parser.parse_args()
+
+    source_lang = args.source_lang
+
+    if source_lang == "en":
+        target_lang = "es"
+        data_file = "dataset_en_train.pkl"
+        model_name = "Helsinki-NLP/opus-mt-en-es"
+    elif source_lang == "es":
+        target_lang = "en"
+        data_name = "dataset_es_train.pkl"
+        model_name = "Helsinki-NLP/opus-mt-es-en"
+    else:
+        raise ValueError("Invalid language, choose 'en' or 'es'")
+
+    print("Loading and filtering data...")
+    df = load_and_filter_data(data_name)
+    df = df.loc[:5].copy()
+    print(f"Data loaded and filtered.\nNumber of sequences in the dataset: {len(df)}")
+    print(f"Translating text from {source_lang} to {target_lang}...")
+    df_translations = translate_text(df)
+    # Save the translated data to a pickle file
+    df_translations.to_pickle(f"./data/pickle/dataset_{target_lang}_backtranslated.pkl")
---
+++ task/data_tools/json_to_pickle.py
@@ -0,0 +1,32 @@
+import os
+import pandas as pd
+import json
+
+def json_to_dataframe(json_file):
+    with open(json_file, 'r', encoding='UTF-8') as f:
+        data = json.load(f)
+    return pd.DataFrame(data)
+
+def convert_json_files_to_pickle(json_dir, pickle_dir):
+    # Create pickle directory if it doesn't exist
+    if not os.path.exists(pickle_dir):
+        os.makedirs(pickle_dir)
+    
+    # Loop through each json file in the directory
+    for filename in os.listdir(json_dir):
+        if filename.endswith('.json'):
+            json_path = os.path.join(json_dir, filename)
+            df = json_to_dataframe(json_path)
+            df = df[['id', 'text']].copy() # select only the 'id' and 'text' columns
+            pickle_path = os.path.join(pickle_dir, os.path.splitext(filename)[0] + '.pkl')
+            df.to_pickle(pickle_path)
+            print(f"Converted {filename} to {pickle_path}")
+
+# Example usage
+if __name__ == "__main__":
+    # Specify the directory containing JSON files and the directory to save pickle files
+    json_directory = "data/"
+    pickle_directory = "data/pickle/"
+    
+    # Convert JSON files to pickle
+    convert_json_files_to_pickle(json_directory, pickle_directory)
