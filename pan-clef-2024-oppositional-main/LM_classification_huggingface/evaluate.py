4ce1a40db56b658b34ba314dd394ae69a5e73000
nikob <nikolaj.bauer@uzh.ch>
2024-04-19 10:06:10 +0200

    removing old folders, created before the merge (they are now in /task)

--- pan-clef-2024-oppositional-main/LM_classification_huggingface/evaluate.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append("..")
-from data_tools.evaluation_utils import evaluate_classif_predictions
-
-
-pred_file = sys.argv[1]
-gold_file = sys.argv[2]
-
-evaluate_classif_predictions(pred_file, gold_file, positive_class="conspiracy")
