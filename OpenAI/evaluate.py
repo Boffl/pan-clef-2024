e6e7d9c3aab2f94b5ee7d6eb3efcdbb2b2e83098
nikob <nikolaj.bauer@uzh.ch>
2024-04-21 17:09:08 +0200

    add MCC

--- OpenAI/evaluate.py
+++ OpenAI/evaluate.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.metrics import precision_score, recall_score, f1_score, matthews_corrcoef
 import sys
 
 gold_file = sys.argv[1]
@@ -15,3 +15,4 @@ pred = open_label_file(pred_file)
 print(f"P: {precision_score(gold, pred)}")
 print(f"R: {recall_score(gold, pred)}")
 print(f"F1: {f1_score(gold, pred)}")
+print(f"MCC: {matthews_corrcoef(gold, pred)}")
