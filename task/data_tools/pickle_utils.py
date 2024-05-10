c7ce1fab3993f79d7261abda33abc9d4f5b538a5
nadiatimoleon <102558021+nadiatimoleon@users.noreply.github.com>
2024-05-11 00:06:39 +0200

    Update pickle_utils.py

--- task/data_tools/pickle_utils.py
+++ task/data_tools/pickle_utils.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 
 
-def load_pickle_to_df(pickle_path="./data/pickle/"):
-  df = pd.read_pickle(pickle_path)
+def load_pickle_to_df(pickle_name, pickle_path="./data/pickle/"):
+  full_path = os.path.join(pickle_path, pickle_name)
+  df = pd.read_pickle(full_path)
   return df
