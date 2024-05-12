6d46117c484f744f69c9fb33eba3d65bdd75545a
nadiatimoleon <102558021+nadiatimoleon@users.noreply.github.com>
2024-05-12 18:58:11 +0200

    clean-ups

--- inspect_translations.py
+++
@@ -1,9 +0,0 @@
-import os
-import glob
-from task.data_tools.pickle_utils import load_pickle_to_df
-
-pickle_path = "./data/pickle/"
-pickle_file = os.path.join(pickle_path, "dataset_ES_translated.pkl")
-
-df = load_pickle_to_df(pickle_file)
-print(df.head(10))
